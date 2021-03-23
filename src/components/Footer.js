78534281318f6eb1513e69359971315c41ab56c8
Yu Chu <cyjeff@gmail.com>
2021-03-23 23:09:49 +0900

    added upload function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,22 @@ import "./footer.css";
 import cam from "./camera.png";
 
 function Footer() {
+  async function upload(e) {
+    const file = e.target.files[0];
+    await fetch(
+      `https://storage.googleapis.com/upload/storage/v1/b/jeffigram/o?uploadType=media&name=${file.name}`,
+      {
+        method: "POST",
+        body: file,
+      }
+    );
+  }
   return (
     <div className="footer">
       <div className="footerBack"></div>
       <div className="camIcon">
         <img className="camImg" alt="cam" src={cam} />
-        <input type="file"></input>
+        <input type="file" onChange={upload}></input>
       </div>
     </div>
   );
