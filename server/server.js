01358adb12a6b2ebfd5f7ab9c400d3da913fd9af
Yu Chu <cyjeff@gmail.com>
2021-03-23 20:45:23 +0900

    minor refactor

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5566;
 
 const config = require("../knexfile");
 const knex = require("knex")(config[process.env.DB_ENV]);
