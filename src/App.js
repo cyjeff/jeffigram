278bfb537c67f9a6e45010b61739466baf853451
Yu Chu <cyjeff@gmail.com>
2021-03-23 10:59:44 +0900

    added header and logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./components/Header";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -19,8 +20,17 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <header className="App-header">{posts}</header>
       <div>Photo</div>
+      <img
+        src="https://storage.googleapis.com/download/storage/v1/b/jeffigram/o/DCE9838B-7CBF-4F0B-9E11-40B9889A2588_1_105_c.jpeg?generation=1616390550076602&alt=media"
+        alt="test"
+      ></img>
+      <img
+        src="https://storage.googleapis.com/download/storage/v1/b/jeffigram/o/DCE9838B-7CBF-4F0B-9E11-40B9889A2588_1_105_c.jpeg?generation=1616390550076602&alt=media"
+        alt="test"
+      ></img>
       <img
         src="https://storage.googleapis.com/download/storage/v1/b/jeffigram/o/DCE9838B-7CBF-4F0B-9E11-40B9889A2588_1_105_c.jpeg?generation=1616390550076602&alt=media"
         alt="test"
